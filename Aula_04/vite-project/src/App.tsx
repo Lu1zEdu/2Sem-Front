183eb9da1a2a3ecaa3e599e420de9f00ee3a6fa5
Lu1zEdu <le6269199@gmail.com>
2024-08-16 07:57:22 -0300

    Update8

---
+++ Aula_04/vite-project/src/App.tsx
@@ -0,0 +1,18 @@
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+
+
+
+export default function App() {
+  //área declarativa
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+
+}
---
+++ Aula_04/vite-project/src/Components/Main.tsx
@@ -0,0 +1,13 @@
+import Conteudo from "./Conteudo";
+
+
+export default function Main() {
+    return (
+        <div>
+            <main>
+                <h1>Principal</h1>
+                <Conteudo />
+            </main>
+        </div>
+    );
+}
