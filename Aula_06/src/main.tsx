4460afc5b4918bc03869860cb0d6c0539746ca9d
Lu1zEdu <le6269199@gmail.com>
2024-08-21 09:03:37 -0300

    Aula06.9

--- Aula_06/src/main.tsx
+++ Aula_06/src/main.tsx
@@ -2,9 +2,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import Home from './Routes/Home/Index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Home />
     <App />
   </StrictMode>,
 )
