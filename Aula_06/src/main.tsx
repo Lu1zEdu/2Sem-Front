8cca6c22424e3905204c9a77da960c269055f278
Lu1zEdu <le6269199@gmail.com>
2024-08-21 09:11:36 -0300

    Aula06.10

--- Aula_06/src/main.tsx
+++ Aula_06/src/main.tsx
@@ -1,12 +1,40 @@
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.tsx'
+import EditarProdutos from './Routes/EditarProdutos/Index.tsx'
+import Error from './Routes/Error/Index.tsx'
 import Home from './Routes/Home/Index.tsx'
+import Produtos from './Routes/Produtos/Index.tsx'
+
+const Routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/Produtos',
+        element: <Produtos />,
+        errorElement: <Error />,
+      },
+      {
+        path: '/Produtos/Editar/:Id',
+        element: <EditarProdutos />,
+        errorElement: <Error />,
+      }
+    ]
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
-    <App />
+    <RouterProvider router={Routes} />
   </StrictMode>,
 )
